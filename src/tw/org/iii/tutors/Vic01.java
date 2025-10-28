a0fa3feefdcac51134b06d759da138153e42ee11
yooooobook <146624730+yooooobook@users.noreply.github.com>
2025-10-28 21:44:38 +0800

    202510282145

---
+++ src/tw/org/iii/tutors/Vic01.java
@@ -0,0 +1,21 @@
+package tw.org.iii.tutors;
+
+public class Vic01 {
+
+	public static void main(String[] args) {
+		// byte, short, int, long
+		// byte¡G 2^8 => -128 ~ 127
+		// short¡G2^16 => -32768 ~ 32767
+		// int¡G2^32 => 
+		// long¡G2^64 => 
+		
+		short var1;
+		var1 = -129;
+		System.out.println(var1);
+		
+		byte var2 = 123;
+		System.out.println(var2);
+
+	}
+
+}
